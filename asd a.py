e14fae8770d121a34f089d065ddc8df2662f1bb2
standinapex1 <44042612+standinapex1@users.noreply.github.com>
2018-10-14 16:25:20 +0530

    Add files via upload

---
+++ asd a.py
@@ -0,0 +1,14 @@
+Python 2.7.3 (default, Apr 10 2012, 23:24:47) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+hello world
+>>> ================================ RESTART ================================
+>>> 
+hello world
+Hello World
+>>> print hello world
+SyntaxError: invalid syntax
+>>> print "hello world"
+hello world
+>>> 
